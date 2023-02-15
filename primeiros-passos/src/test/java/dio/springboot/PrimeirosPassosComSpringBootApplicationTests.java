0328e0e0e539f9d16185b77dbe5813149f0d795a
izaidetenutes <izaide.tenutes@id-cotton.com>
2023-02-15 17:59:45 -0400

    Fisrt Commit

---
+++ primeiros-passos/src/test/java/dio/springboot/PrimeirosPassosComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
